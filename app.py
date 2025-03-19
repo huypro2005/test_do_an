489d353cf40012ad08e1632c0340f9dfeb50b422
Cao Thanh Huy <23520595@gm.uit.edu.vn>
2025-03-19 16:22:06 +0700

    Tạo login nhanvien

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+from email import message
 from struct import calcsize
-from flask import Flask
+from flask import Flask, jsonify, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate, migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from sqlalchemy import false, true
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///a.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'hello_man'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+requests_count = {}
+
 
 # QĐ1: Có 10 sân bay. Thời gian bay tối thiểu là 30 phút. Có tối đa 2 sân bay trung gian với thời gian dừng từ 10 đến 20 phút.
 # QĐ2: Chỉ bán vé khi còn chỗ. Có 2 hạng vé (1, 2). Vé hạng 1 bằng 105% của đơn giá, vé hạng 2 bằng với đơn giá, mỗi chuyến bay có một giá vé riêng.
@@ -18,35 +27,104 @@ db = SQLAlchemy(app)
 
 class Nhanvien(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), unique = True)
+    name = db.Column(db.String(80))
+    username = db.Column(db.String(50), unique = True)
+    password = db.Column(db.Text)
+    email = db.Column(db.String(120), unique = True)
+    pos = db.Column(db.Integer)
 
     def __repr__(self):
         return self.name
-
-    def add(self, name):
+    
+    def create_user(self, name, username, password, email):
+        self.name = name
+        self.username = username
+        self.password = password 
+        self.email = email
         try:
-            self.name = name
             db.session.add(self)
             db.session.commit()
             return True
         except:
             return False
-        
-
-class a:
-    pass
-
-class b:
-    # code
-    def i(a:a):
-        pass
-
-class a:
-    # code
-    def i(self):
-        pass
 
 with app.app_context():
     db.create_all()
+    db.session.commit()
 
-# a=  Nhanvien.query.get(1)
+
+def log_count():
+    ip = request.remote_addr
+    if ip in requests_count:
+        requests_count[ip] +=1
+    else:
+        requests_count[ip] =1
+    print(f'{ip} requested')
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    log_count()
+    try:
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user = Nhanvien.query.filter_by(username = username).first()
+        if check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            session['username'] = user.username
+            session['email'] = user.email
+            session['name'] = user.name
+            return {'status': 'success', 'message': 'Login successful'}
+        else:
+            return jsonify({'status': 'error', 'message': 'Invalid username or password'})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+    
+@app.route('/api/logout', methods=['POST'])
+def logout():
+    log_count()
+    if 'user_id' in session:
+        session.clear()
+        return jsonify({'message' : 'Logout success'})
+    return jsonify({'message': "logout fail"})
+
+
+@app.route('/api/register', methods =['POST'])
+def register():
+    log_count()
+    data = request.get_json()
+    tmp = ['username', 'password', 'name', 'email']
+    if any(i not in data for i in tmp):
+        return jsonify({'message': 'Thiếu thông tin kìa cu'})
+    username = data['username']
+    password = data['password']
+    hashpass = generate_password_hash(password)
+    email = data['email']
+    name = data['name']
+    user =Nhanvien()
+    if user.create_user(name, username, hashpass, email):
+        return jsonify({'message': 'success'})
+    return jsonify({'message': 'fail'})
+
+@app.route('/api/get_account/')
+def get_acc():
+    log_count()
+    if 'username' in session:
+        return jsonify({
+            'username' :  session['username'],
+            'email' : session['email'],
+            'name': session.get('name')
+        })
+    return jsonify({'message': 'chưa login'})
+
+
+# {
+#     "username": "huypro37",
+#     "password": "123456",
+#     "email": "111",
+#     "name": "huy"
+# }
+
+if __name__ == '__main__':
+    app.run(debug=True)
